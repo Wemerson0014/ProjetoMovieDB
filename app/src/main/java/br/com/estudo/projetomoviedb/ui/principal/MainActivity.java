83dfda2a5cad6c6999681808876e4f9308e3db86
Wemerson0014 <wemersonf.rangel@outlook.com>
2019-06-15 14:58:24 -0300

    Toolbar
    * Adiciona componente toolbar para activity_main.xml
    * Implementa recursos para exibicao da toolbar na tela principal

--- app/src/main/java/br/com/estudo/projetomoviedb/ui/principal/MainActivity.java
+++ app/src/main/java/br/com/estudo/projetomoviedb/ui/principal/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int CONTEUDO_PRINCIPAL = 1;
     private ViewFlipper viewFlipperPrincipal;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewFlipperPrincipal = findViewById(R.id.viewFlipperPrincipal);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.text_toolbar_principal);
         buscaFilmes();
 
     }
